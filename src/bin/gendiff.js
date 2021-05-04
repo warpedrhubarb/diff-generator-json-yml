919a082569663a012ae1c251f24d99d5cf488948
warpedrhubarb <haveanicedayy420@gmail.com>
2021-05-04 17:16:50 +0300

    Update gendiff.js (args & opts)

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { Command } from 'commander';
 
 const program = new Command();
@@ -5,5 +7,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
